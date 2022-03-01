cd6cc250814483be70e5fc5a02aff0632f52ec43
Matt-Tanti <m.tanti02@gmail.com>
2022-03-01 23:40:56 +0100

    Binding Setup

--- app/src/main/java/io/github/lkleno/mobilesensing/MainActivity.kt
+++ app/src/main/java/io/github/lkleno/mobilesensing/MainActivity.kt
@@ -2,10 +2,16 @@ package io.github.lkleno.mobilesensing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.lkleno.mobilesensing.databinding.ActivityMainBinding
+
+private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
